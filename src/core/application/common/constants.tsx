8a3abf225cdd439dcb91545aa03f83e9f43f0f75
tuyenpn <DT050236@actvn.edu.vn>
2023-07-02 23:29:49 +0700

    fix

--- src/core/application/common/constants.tsx
+++ src/core/application/common/constants.tsx
@@ -11,8 +11,8 @@ import iconAccount from 'assets/icons/icon-account.png'
 import iconSubject from 'assets/icons/icon-subject.png'
 import iconSetting from 'assets/icons/icon-setting.png'
 import Menu from "@/core/application/common/models/LeftMenu/Menu";
-import MenuItem from "@/core/application/common/models/LeftMenu/MenuItem";
-import { MenuKeys } from "@/core/domain/enums/MenuKeys";
+import MenuItem from "src/core/application/common/models/LeftMenu/MenuItem";
+import { MenuKeys } from "src/core/domain/enums/MenuKeys";
 import type { NotificationPlacement } from 'antd/es/notification/interface';
 import GroupedMenuItem from "./models/LeftMenu/GroupedItem";
 
